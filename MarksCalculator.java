a54a8221a26948c268aba7177377fbe2b0c4c715
NikhilQuery <nainshab1357@gmail.com>
2026-09-18 12:25:14 +0530

    Refactored MarksCalculator using return statement and logic gates

--- MarksCalculator.java
+++ MarksCalculator.java
@@ -5,14 +5,39 @@ public class MarksCalculator {
         Scanner sc = new Scanner(System.in);
         System.out.print("enter first subject marks: ");
         double mark1 = sc.nextDouble();
+        if (mark1 > 100 || mark1 < 0) {
+            System.out.println("Error:Marks must be between 0 and 100.Please start program again");
+            sc.close();
+            return; 
+        }
         System.out.print("enter second subject marks: ");
         double mark2 = sc.nextDouble();
+        if (mark2 > 100 || mark2 < 0) {
+            System.out.println("Error: Invalid Marks! Marks must be between 0 and 100.Please start program again");
+            sc.close();
+            return; 
+        }
         System.out.print("enter third subject marks: ");
         double mark3 = sc.nextDouble();
+        if (mark3 > 100 || mark3 < 0) {
+            System.out.println("Error: Invalid Marks! Marks must be between 0 and 100.Please start program again");
+            sc.close();
+            return; 
+        }
         System.out.print("enter fourth subject marks: ");
         double mark4 = sc.nextDouble();
+        if (mark4 > 100 || mark4 < 0) {
+            System.out.println("Error: Invalid Marks! Marks must be between 0 and 100.Please start program again");
+            sc.close();
+            return; 
+        }
         System.out.print("enter fifth subject marks: ");
         double mark5 = sc.nextDouble();
+        if (mark5 > 100 || mark5 < 0) {
+            System.out.println("Error: Invalid Marks! Marks must be between 0 and 100.Please start program again");
+            sc.close();
+            return; 
+        }
         double totalobtained = mark1 + mark2 + mark3 + mark4 + mark5 ;
         double totalmarks = 500.0;
         double percentage = (totalobtained / totalmarks) * 100;
